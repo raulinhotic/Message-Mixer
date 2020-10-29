f4805e270573f6b8ecbbc575165ec403670ee8c4
Raulinhotic <schluet0r4life@gmail,com>
2020-10-29 17:06:31 +0100

    created the object containing arrays with several footballing quotes

---
+++ script.js
@@ -0,0 +1,7 @@
+const genericFootballQuotes = {
+    firstArray: ["Wir haben heute gegen einen starken Gegner gespielt.", "Es ist schwer direkt nach dem Spiel etwas zu sagen.", "Ich muss der Mannschaft ein großes Kompliment aussprechen.", "Das war heute eine reife Leistung.", "Unter diesen Umständen müssen wir mit dem Ergebnis zufrieden sein."],
+    secondArray: ["Das war ein typisches Kampfspiel.", "Der Gegner hat es uns heute nicht leicht gemacht.", "Wir müssen uns weiterhin auf unsere eigene Leistung fokussieren.", "Heute war das Glück nicht immer auf unserer Seite", "Wir müssen die Fehler unbedingt abstellen"],
+    thirdArray: ["Wir schauen von Spiel zu Spiel.", "Das nächste Spiel wird auch schwer.", "Wir müssen uns jetzt erst einmal schütteln.", "Solche Spiele passieren.", "Wir müssen das nächste Spiel eine Schippe drauflegen."]
+    
+};
+
