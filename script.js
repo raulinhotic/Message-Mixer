e86bc0df8aeda671e05608848d698445ab63c5a5
Raulinhotic <schluet0r4life@gmail,com>
2020-10-29 17:19:00 +0100

    added functionality to pick random quotes and form the message

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const genericFootballQuotes = {
     firstArray: ["Wir haben heute gegen einen starken Gegner gespielt.", "Es ist schwer direkt nach dem Spiel etwas zu sagen.", "Ich muss der Mannschaft ein großes Kompliment aussprechen.", "Das war heute eine reife Leistung.", "Unter diesen Umständen müssen wir mit dem Ergebnis zufrieden sein."],
     secondArray: ["Das war ein typisches Kampfspiel.", "Der Gegner hat es uns heute nicht leicht gemacht.", "Wir müssen uns weiterhin auf unsere eigene Leistung fokussieren.", "Heute war das Glück nicht immer auf unserer Seite", "Wir müssen die Fehler unbedingt abstellen"],
-    thirdArray: ["Wir schauen von Spiel zu Spiel.", "Das nächste Spiel wird auch schwer.", "Wir müssen uns jetzt erst einmal schütteln.", "Solche Spiele passieren.", "Wir müssen das nächste Spiel eine Schippe drauflegen."]
+    thirdArray: ["Wir schauen von Spiel zu Spiel.", "Das nächste Spiel wird auch schwer.", "Wir müssen uns jetzt erst einmal schütteln.", "Solche Spiele passieren.", "Wir müssen das nächste Spiel eine Schippe drauflegen."],
+
+    generateMessage: function () {
+        const firstQuote = this.firstArray[Math.floor(Math.random() * this.firstArray.length)];
+        const secondQuote = this.secondArray[Math.floor(Math.random() * this.secondArray.length)];
+        const thirdQuote = this.thirdArray[Math.floor(Math.random() * this.thirdArray.length)];
+
+        return `Random Generic Football Quote: ${firstQuote} ${secondQuote} ${thirdQuote}`;
+        
+    }
     
 };
 
+console.log(genericFootballQuotes.generateMessage());
